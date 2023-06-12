26513e489e2c3ef476ef10cdbc6eca9b2897c982
maria <beezzzmv@gmail.com>
2023-06-12 19:11:38 +0300

    change

--- page.js
+++ page.js
@@ -1,92 +1,121 @@
 
 var modal = document.getElementById("myModal");
-
-var btn = document.getElementsByClassName('close');
-var cases = document.getElementsByClassName('case')
-var card = document.getElementsByClassName('card')
+var content = document.getElementsByClassName('content')[0]
+var btn = document.getElementsByClassName('close')[0];
+var cases = document.querySelectorAll('.case')
+var cards = document.querySelectorAll('.card')
+const timeout = 800
 
 btn.onclick = function () {
-    modal.style.display = 'none'
+    modal.classList.remove('open')
 }
 window.onclick = function (event) {
     if (event.target == modal) {
-        modal.style.display = "none";
+        modal.classList.remove('open')
     }
 }
 
-for (let i = 0; i < cases.length; i++) {
-    cases[i].onclick = function () {
-        modal.style.display = 'block'
-    }
-}
 
-for (let i = 0; i < card.length; i++) {
-    card[i].onclick = function () {
-        modal.style.display = 'block'
-    }
-}
+cases.forEach(function (item) {
+    item.addEventListener('click', function () {
+        modal.classList.add('open')
+        content.innerHTML = item.getAttribute('data-text')
+    })
+})
 
+cards.forEach(function (item) {
+    item.addEventListener('click', function () {
+        modal.classList.add('open')
+        content.innerHTML = item.getAttribute('data-text')
+    })
+})
+
+
+$('.slider1').slick({
+    arrows: true,
+    slidesToShow: 4,
+    initialSlide: 0,
+    appendArrows: $('.arrows'),
+    responsive: [
+        {
+            breakpoint: 1000,
+            settings: {
+                slidesToShow: 3,
+            }
+        },
+        {
+            breakpoint: 800,
+            settings: {
+                slidesToShow: 2,
+            }
+        },
+    ]
+
+});
 
 
 $(document).ready(function () {
     $('.slider').slick({
-        arrows: false,
-        slidesToShow: 3,
+        slidesToShow: 4,
+        arrows: true,
+        initialSlide: 0,
+        appendArrows: $('.arrowsservices'),
         responsive: [
             {
                 breakpoint: 1000,
                 settings: {
-                    slidesToShow: 2,
+                    slidesToShow: 3,
                 }
-            }
-        ]
-    });
-
-
-    $('.slider1').slick({
-        arrows: false,
-        slidesToShow: 3,
-        responsive: [
+            },
             {
-                breakpoint: 1000,
+                breakpoint: 800,
                 settings: {
                     slidesToShow: 2,
                 }
-            }
+            },
         ]
-
     });
 
     $('.slider3').slick({
-        arrows: false,
-        slidesToShow: 3,
+        slidesToShow: 4,
+        initialSlide: 0,
+        appendArrows: $('.arrowsBlog'),
+        arrows: true,
         responsive: [
             {
                 breakpoint: 1000,
                 settings: {
+                    slidesToShow: 3,
+                }
+            },
+            {
+                breakpoint: 800,
+                settings: {
                     slidesToShow: 2,
                 }
-            }
+            },
         ]
     });
+
+
     $('.carousel-vertical').slick({
         arrows: false,
         slidesToShow: 4,
         vertical: true,
+        infinite: true,
+        cssEase: 'linear',
         autoplay: true,
-        autoplaySpeed: 500,
-        infinite: true
+        autoplaySpeed: 0,
+        swipeToSLide: true,
     });
 })
 
 
+
+
+// mobile menu
 let menuBtn = document.querySelector('.menu-btn');
 let menu = document.querySelector('.menu');
 menuBtn.addEventListener('click', function () {
     menu.classList.toggle('active');
 })
-
-// menuBtn.addEventListener('click', function(){
-// 	menuBtn.classList.toggle('active');
-// 	menu.classList.toggle('active');
-// })
